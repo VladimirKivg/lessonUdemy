daa26e0638b866640bd0da8b39f5f4f7995691a2
VladimirKing <cheremuha1@gmail.com>
2021-04-17 21:12:45 +0300

    udemy

---
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/controller/MyController.java
@@ -0,0 +1,25 @@
+package com.vova.spring.mvc.hibernate_aop.controller;
+
+import com.vova.spring.mvc.hibernate_aop.dao.EmployeeDao;
+import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    @RequestMapping("/")
+    public String shoeAllEmployees(Model model){
+        List<Employee> allEmployees = employeeDao.getAllEmployees();
+
+model.addAttribute("allEmps",allEmployees);
+        return "all-employees";
+    }
+}
---
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.vova.spring.mvc.hibernate_aop.dao;
+
+import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+List<Employee> getAllEmployees();
+}
