13cdbf6bae7a995710f2cbf664a5246806b1fa10
VladimirKing <cheremuha1@gmail.com>
2021-04-24 12:19:03 +0300

    service impl

--- src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeServiceImpl.java
@@ -1,12 +1,18 @@
 package com.vova.spring.mvc.hibernate_aop.service;
 
+import com.vova.spring.mvc.hibernate_aop.dao.EmployeeDao;
 import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeDao  employeeDao;
+
     @Override
     public List<Employee> getAllEmployees() {
         return null;
