c9713c97a0e26fe852fe332dd1292e64b79ee92b
VladimirKing <cheremuha1@gmail.com>
2021-04-24 12:12:19 +0300

    service impl

--- src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeServiceImpl.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeServiceImpl.java
@@ -1,4 +1,12 @@
 package com.vova.spring.mvc.hibernate_aop.service;
 
-public class EmployeeServiceImpl {
+import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
 }
