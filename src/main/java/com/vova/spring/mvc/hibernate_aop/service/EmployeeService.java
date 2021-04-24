156cf47f787483b9f1db1b6aa03279a5a6540422
VladimirKing <cheremuha1@gmail.com>
2021-04-24 12:12:00 +0300

    service

--- src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeService.java
@@ -1,4 +1,9 @@
 package com.vova.spring.mvc.hibernate_aop.service;
 
+import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    public List<Employee> getAllEmployees();
 }
