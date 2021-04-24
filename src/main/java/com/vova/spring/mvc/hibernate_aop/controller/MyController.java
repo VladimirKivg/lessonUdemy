01abfbe3562864f8c195a86f13c0e9a55ec7ce4b
VladimirKing <cheremuha1@gmail.com>
2021-04-24 12:35:09 +0300

    controller  in service

--- src/main/java/com/vova/spring/mvc/hibernate_aop/controller/MyController.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/controller/MyController.java
@@ -2,6 +2,7 @@ package com.vova.spring.mvc.hibernate_aop.controller;
 
 import com.vova.spring.mvc.hibernate_aop.dao.EmployeeDao;
 import com.vova.spring.mvc.hibernate_aop.entity.Employee;
+import com.vova.spring.mvc.hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,14 +14,18 @@ import java.util.List;
 @Controller
 public class MyController {
 
+//    @Autowired
+//    private EmployeeDao employeeDao;
+
     @Autowired
-    private EmployeeDao employeeDao;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String shoeAllEmployees(Model model){
-        List<Employee> allEmployees = employeeDao.getAllEmployees();
+//        List<Employee> allEmployees = employeeDao.getAllEmployees();
 
-model.addAttribute("allEmps",allEmployees);
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        model.addAttribute("allEmps",allEmployees);
         return "all-employees";
     }
 }
