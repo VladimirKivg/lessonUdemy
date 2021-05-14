7506bab6789f994570f0f4e993c47665e7bd5e49
VladimirKing <cheremuha1@gmail.com>
2021-05-14 12:59:49 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
@@ -1,4 +1,13 @@
 package com.vova.spring.mvc.hibernate_aop.aspect;
 
+import org.aspectj.lang.annotation.Around;
+
 public class MyLoggingAspect {
+
+    @Around("execution (* com.vova.spring.mvc.hibernate_aop)")
+    public Object aroundAllRepositoryMethodsAdvice (){
+
+        return null;
+    }
+
 }
