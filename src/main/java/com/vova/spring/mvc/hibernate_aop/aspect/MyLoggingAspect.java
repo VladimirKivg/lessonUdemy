53b3f58f2f90bc7c57976ebc639bad8f717a7373
VladimirKing <cheremuha1@gmail.com>
2021-05-14 13:06:01 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
@@ -1,11 +1,12 @@
 package com.vova.spring.mvc.hibernate_aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 
 public class MyLoggingAspect {
 
     @Around("execution (* com.vova.spring.mvc.hibernate_aop.dao.*.*(..))")
-    public Object aroundAllRepositoryMethodsAdvice (){
+    public Object aroundAllRepositoryMethodsAdvice (ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
         return null;
     }
