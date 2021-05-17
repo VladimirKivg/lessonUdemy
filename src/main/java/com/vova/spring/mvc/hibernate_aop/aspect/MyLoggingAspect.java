7fc1a5f79099b1740c8b9acbaed39d0719ec0043
VladimirKing <cheremuha1@gmail.com>
2021-05-17 14:19:14 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.stereotype.Component;
+import org. springframework.stereotype.Component;
 
 @Component
 @Aspect
