1fa279fb3973dc65e4422bd03486e46e78942146
VladimirKing <cheremuha1@gmail.com>
2021-05-17 14:21:59 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
@@ -12,7 +12,7 @@ public   class     MyLoggingAspect {
 
     @Around("execution   (* com.vova.spring.mvc.hibernate_aop.dao.*.*(..))")
     public Object   aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        MethodSignature  methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         String   methodMame =  methodSignature.getName();
         System.out.println("begin OF " + methodMame);
 
