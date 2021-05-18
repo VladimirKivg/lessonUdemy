ff48796ae4b435c5f2ebe795b403a810dc2249ae
VladimirKing <cheremuha1@gmail.com>
2021-05-18 19:51:34 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/service/EmployeeService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface EmployeeService {
     public List<Employee> getAllEmployees();
 
-    public void   saveEmployee(Employee employee);
+    public void     saveEmployee(Employee employee);
 
     public Employee getEmployee(int id);
 
