54564300e54f96b640f1436255d85e04cd71093e
VladimirKing <cheremuha1@gmail.com>
2021-06-09 12:58:29 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/dao/EmployeeDao.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/dao/EmployeeDao.java
@@ -5,12 +5,12 @@ import com.vova.spring.mvc.hibernate_aop.entity.Employee;
 import java.util.List;
 
 
-public interface                                 EmployeeDao {
+public interface EmployeeDao {
     List<Employee> getAllEmployees();
 
     public void saveEmployee(Employee employee);
 
-    public Employee  getEmployees(int id);
+    public Employee getEmployees(int id);
 
     public void deleteEmployee(int id);
 }
