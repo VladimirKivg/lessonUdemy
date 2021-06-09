b71481c01dda8ecbbbf24f29808a7bcf6365863d
VladimirKing <cheremuha1@gmail.com>
2021-06-09 12:57:56 +0300

    work

--- src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/vova/spring/mvc/hibernate_aop/aspect/MyLoggingAspect.java
@@ -4,21 +4,21 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org. springframework.stereotype.Component;
+import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public   class              MyLoggingAspect {
+public class MyLoggingAspect {
 
     @Around("execution                                 (* com.vova.spring.mvc.hibernate_aop.dao.*.*(..))")
-    public Object   aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        MethodSignature  methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
-        String   methodMame =  methodSignature.getName();
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodMame = methodSignature.getName();
         System.out.println("begin OF " + methodMame);
 
         Object targetMethodResult = proceedingJoinPoint.proceed();
 
-        System.out.println ("End of" +  methodMame);
+        System.out.println("End of" + methodMame);
 
         return targetMethodResult;
     }
